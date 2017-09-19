ad773a5ca2f6b85de78042bd91727642709cbe5c
Bryn Beaudry <babeaudry@gmail.com>
2017-09-19 13:13:55 -0700

    trying to deploy

---
+++ Seminar4/Models/Validation/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Seminar4.Models.NorthWind;
+
+namespace Seminar4.Models
+{
+
+    [MetadataType(typeof(CategoryMetaData))]
+    public partial class Category
+    {
+        public static implicit operator Category(Seminar4.Models.NorthWind.Category v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CategoryMetaData
+    {
+        [Required]
+        [Display(Name = "Category Name")]
+        public object CategoryName { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "The {0} must be between {2} and {1} characters.", MinimumLength = 5)]
+        public object Description { get; set; }
+    }
+}
