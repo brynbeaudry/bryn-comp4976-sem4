0e20d69758c9f2981008720ab000f518c20d68c8
Bryn Beaudry <babeaudry@gmail.com>
2017-09-19 11:45:01 -0700

    Add project files.

---
+++ Seminar4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Seminar4.MvcApplication" Language="C#" %>
---
+++ Seminar4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Seminar4.Startup))]
+namespace Seminar4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
